c8a34fb96acd9b59ea5e78337583a5c08dde0f67
dnatuna <dnatuna123@gmail.com>
2022-02-09 00:25:26 +0900

    Feat: /hello api 개발 및 테스트 코드 작성

---
+++ SpringBootAWSBook/src/main/java/fun/dnatuna/bootstudy/web/HelloController.java
@@ -0,0 +1,13 @@
+package fun.dnatuna.bootstudy.web;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+    @GetMapping("/hello")
+    public String Hello() {
+        return "Hello";
+    }
+}
